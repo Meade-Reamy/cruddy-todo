cea7b0cd884aa2c51bf1bda8b9edf62aea0e13e3
davetwo7 <davidolu2019@gmail.com>
2023-06-06 13:45:11 -0400

    Complete retrieving all todos

--- datastore/index.js
+++ datastore/index.js
@@ -29,17 +29,22 @@ exports.readAll = (callback) => {
       throw ('error reading files');
     }
     let data = [];
-    for (let idx = 1; idx <= count; idx++) {
+    if (count === 0) { // if there are no todos to read
+      callback(err, data);
+    }
+    for (let idx = 1; idx <= count; idx++) { // if there is data to read
       fs.exists(exports.dataDir + `/${counter.zeroPaddedNumber(idx)}.txt`, (e) => {
         if (e) {
-          data.push(exports.dataDir + `/${counter.zeroPaddedNumber(idx)}.txt`);
-          console.log(data);
+          let id = counter.zeroPaddedNumber(idx);
+          data.push({ id: id, text: id });
+        }
+        if (data.length === count) {
+          callback(err, data);
         }
       });
-      console.log(data);
     }
-    console.log(data);
-    callback(err, data);
+    // console.log('data outside', data);
+    // callback(err, data);
   });
 
   // var data = _.map(items, (text, id) => {
